f7fcb7933617bb8dec0f0942d7832fe84e188369
Ankit Dandotiya <dandotiyaankit07@gmail.com>
2026-07-30 22:22:08 +0530

    Comment Added

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/user");
 const {handleUserSignup,handleUserLogin} = require("../controllers/user");
 
+// on /user -> handleUserSignup executed And on /user/login -> handleUserLogin executed
 router.post('/',handleUserSignup);
 router.post('/login',handleUserLogin);
 
