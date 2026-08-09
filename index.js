e444aa0bcfe0925b8761fea38b61dcf30deaefa3
Ankit Dandotiya <dandotiyaankit07@gmail.com>
2026-08-09 23:22:31 +0530

    Comments Added

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ connectToDB("mongodb://localhost:27017/short-url").then(() => {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-// Middlewares 
+// Middlewares Fumctions
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
